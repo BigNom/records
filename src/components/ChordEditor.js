692a6f20aa0c09ba344414beea2897eb09c61005
[BigNom] <[simon.biddle@live.com]>
2017-08-27 13:58:31 +0800

    initial commit

--- src/components/ChordEditor.js
+++ src/components/ChordEditor.js
@@ -1,43 +1,55 @@
 import React, { Component } from 'react';
-import ChordSheetJS from 'chordsheetjs'
+import { Link } from 'react-router-dom';
+import ChordSheetJS from 'chordsheetjs';
 
 class ChordEditor extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { value: 'Type some lyrics here '};
   }
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
+  handleChange(event) {
+    const song = { ...this.props.item };
+    song.chordpro = event.target.value;
+
+    this.props.updateSong(song);
   }
 
   getChordMarkup() {
-    var formatter = new ChordSheetJS.HtmlFormatter(),
-    parser = new ChordSheetJS.ChordProParser(),
-    song = parser.parse(this.state.value);
+    const formatter = new ChordSheetJS.HtmlFormatter();
+    const parser = new ChordSheetJS.ChordProParser();
+    const song = parser.parse(this.props.item.chordpro);
 
     return { __html: formatter.format(song) };
   }
 
   render() {
+    const { item: song } = this.props
+
     return (
-    <div className="chord-editor">
-      <div className="panel">
-        <h3>Input</h3>
-        <textarea
-        style={{width: "100%", height: "100%"}}
-        onChange={this.handleChange}
-        defaultValue={this.state.value}/>
+      <div>
+        <ul className="pt-breadcrumbs">
+          <li><Link to="/songs" className="pt-breadcrumb">Songs</Link></li>
+          <li><Link to="#" className="pt-breadcrumb">{song.title}</Link></li>
+        </ul>
+        <h2 style={{ margin: "0.5em 0" }}>{song.title}</h2>
+        <div className="chord-editor">
+          <div className="panel">
+            <h3>Input</h3>
+            <textarea
+              style={{ width: "100%", height: "100%" }}
+              onChange={this.handleChange}
+              value={song.chordpro} />
+          </div>
+          <div className="panel">
+            <h3>Output</h3>
+            <div
+              style={{ width: "100%", height: "100%", fontFamily: "monospace" }}
+              className="chord-output"
+              dangerouslySetInnerHTML={this.getChordMarkup()} />
+          </div>
+        </div>
       </div>
-      <div className="panel">
-        <h3>output</h3>
-        <div
-        style={{width: "100%", height: "100%", fontFamily: "monospace"}}
-        className="chord-output"
-        dangerouslySetInnerHTML={this.getChordMarkup()}/>
-      </div>
-    </div>
     );
   }
 }
